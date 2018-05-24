9ce4b4aff3c18833c61b4e6370acace620f897fa
Amy Eckert <amy@amyeckertphoto.com>
2018-05-24 16:45:45 -0400

    adjustments to layout and styling

--- random-country/js/app.js
+++ random-country/js/app.js
@@ -9,15 +9,34 @@
         var copyright = $('p.copyright').text();
         var copyrightNotice = '© ' + year + ' Amy Eckert';
         $('p.copyright').html(copyrightNotice);   
-        console.log(date);
 
         //------ choose random country ----------------------------//
         $(".choose").on("click", function(event) {
         	event.preventDefault();
-        	console.log("button clicked!");
+        	console.log("clicked");
+
+        	var $flag = $('#flag'); 
+        	var $country = $('#country-name');   
+        	var $capital = $('#capital');
+
+        	 // call to API 
+        	$.ajax({
+		        url: "https://restcountries.eu/rest/v2/all"
+		    }).then(function(countries) {
+		    	// choose a random country and show name, flag, capital, and current weather.
+		    	var randomCountry = Math.floor(Math.random() * countries.length);
+		    	var countryIndex = countries[randomCountry]; 	
+		    	var capital = countryIndex.capital;
+
+		    	$country.html(countryIndex.name);
+		    	$flag = $($flag).attr('src', countryIndex.flag);
+				$capital.html(capital);
+
+				console.log(countries);
+		    });	        		
 
         });
 
     });//________THE END________________________________________________________________________________________
 
-})(jQuery);
+})(jQuery);  
