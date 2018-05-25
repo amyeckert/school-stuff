648624a23590220f483366bacc26a0ef9c161b9e
Amy Eckert <amy@amyeckertphoto.com>
2018-05-25 12:32:12 -0400

    tweeting works, styles basically done

--- random-country/js/app.js
+++ random-country/js/app.js
@@ -5,19 +5,20 @@
         //----- update copyright site-wide -----------------------//
         var date = new Date();
         var year = date.getFullYear();
-
         var copyright = $('p.copyright').text();
         var copyrightNotice = '© ' + year + ' Amy Eckert';
-        $('p.copyright').html(copyrightNotice);   
+        $('p.copyright').html(copyrightNotice); 
+
+        // ----- Global variables ------------//
+    	var $flag = $('#flag'); 
+    	var $country = $('#country-name');   
+    	var $capital = $('#capital');
+    	var $twitter = $(".twitter-share-button");
 
         //------ choose random country ----------------------------//
         $(".choose").on("click", function(event) {
         	event.preventDefault();
         	console.log("clicked");
-
-        	var $flag = $('#flag'); 
-        	var $country = $('#country-name');   
-        	var $capital = $('#capital');
 
         	 // call to API 
         	$.ajax({
@@ -29,14 +30,19 @@
 		    	var capital = countryIndex.capital;
 
 		    	$country.html(countryIndex.name);
-		    	$flag = $($flag).attr('src', countryIndex.flag);
+		    	$flag = $($flag).attr("src", countryIndex.flag);
 				$capital.html(capital);
 
-				console.log(countries);
+				//-------- get country and location to paste into a tweet-----------------//
+
+				let textToCopy = "I want to go to " + capital + ", " + countryIndex.name + "!";
+		  		
+		  		$twitter= $($twitter).attr("href", "https://twitter.com/intent/tweet?text=" + textToCopy + "");
+				console.log($twitter, textToCopy);
 		    });	        		
 
         });
 
-    });//________THE END________________________________________________________________________________________
+    });//________THE END_________________________________________________________//
 
 })(jQuery);  
